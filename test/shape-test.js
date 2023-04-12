4f4116bd54681ae0086e1c199ebf65cc61c84ca7
Gourab Porey <gourabporey@users.noreply.github.com>
2023-04-12 12:54:29 +0530

    Added square of given length to be tested

--- test/shape-test.js
+++ test/shape-test.js
@@ -1,3 +1,8 @@
+const test = require("../lib/testing.js");
+const shapes = require("../src/shapes/square.js");
+
+const square = shapes.square;
+
 const testSquare = function() {
   test.assertEquality("****\n****\n****\n****\n", square(4), 'should give square of given length');
 }
